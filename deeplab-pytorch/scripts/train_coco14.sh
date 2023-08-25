18fd737b16c6e231da53dfeeda2fd13265d05008
lucasdavid <1595030+lucasdavid@users.noreply.github.com>
2023-08-24 22:55:09 -0300

    Add deeplab-pytorch

---
+++ deeplab-pytorch/scripts/train_coco14.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=48
+#SBATCH -p sequana_gpu_shared
+#SBATCH -J dv2-voc
+#SBATCH -o /scratch/lerdl/lucas.david/logs/%j-dv2-coco.out
+#SBATCH --time=32:00:00
+
+# Copyright 2023 Lucas Oliveira David
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# Train a DeepLabV2 model to perform semantic segmentation.
+#
+
+if [[ "`hostname`" == "sdumont"* ]]; then
+  ENV=sdumont
+  WORK_DIR=$SCRATCH/deeplab-pytorch
+
+  ### Sdumont
+  PY=python3.8
+  PIP=pip3.8
+  DEVICES=0,1,2,3
+
+  # export OMP_NUM_THREADS=4
+  nodeset -e $SLURM_JOB_NODELIST
+  module load sequana/current
+  module load gcc/7.4_sequana python/3.8.2_sequana cudnn/8.2_cuda-11.1_sequana
+else
+  ENV=local
+  WORK_DIR=/home/ldavid/workspace/repos/research/wsss/deeplab-pytorch
+
+  ### Local
+  PY=python
+  PIP=pip
+  DEVICES=0
+fi
+
+cd $SCRATCH/deeplab-pytorch
+
+# echo "Installing dependencies... "
+# $PIP -qq install click tqdm addict joblib omegaconf opencv-python torchnet tensorboard
+# echo "Done."
+
+EXP_ID=coco14
+
+IMAGES_DIR=data/datasets/coco14/JPEGImages
+TXT_FILE=data/datasets/coco14/val.txt
+MODEL_PATH=data_coco/models/$EXP_ID/deeplabv2_resnet101_msc/train_aug/checkpoint_final.pth
+PRED_DIR=data_coco/features/$EXP_ID/deeplabv2_resnet101_msc/test/preds
+
+CUDA_VISIBLE_DEVICES=$DEVICES $PY main.py train  --config-path configs/$EXP_ID.yaml
+CUDA_VISIBLE_DEVICES=$DEVICES $PY main.py test   --config-path configs/$EXP_ID.yaml --model-path $MODEL_PATH
+CUDA_VISIBLE_DEVICES=$DEVICES $PY main.py crf    --config-path configs/$EXP_ID.yaml
+
+CUDA_VISIBLE_DEVICES=$DEVICES $PY demo.py single --config-path configs/$EXP_ID.yaml --model-path $MODEL_PATH \
+  --img-dir $IMAGES_DIR --txt-file $TXT_FILE --save-dir $PRED_DIR --crf
