bc10376462ffc0602ffd3972ae68c25d416a7b44
lucasdavid <1595030+lucasdavid@users.noreply.github.com>
2023-10-25 12:21:37 -0300

    Add sdumont_nvidia env

--- runners/config/env.sh
+++ runners/config/env.sh
@@ -16,6 +16,23 @@ if [[ $ENV == sdumont ]]; then
   nodeset -e $SLURM_JOB_NODELIST
   module load sequana/current
   module load gcc/7.4_sequana python/3.8.2_sequana cudnn/8.2_cuda-11.1_sequana
+elif [[ $ENV == sdumont_nvidia ]]; then
+  nodeset -e $SLURM_JOB_NODELIST
+
+  echo "Loading modules deepl/deeplearn-py3.7"
+  module load deepl/deeplearn-py3.7
+
+  DATA_DIR=$SCRATCH/datasets
+  BUILD_DIR=$SCRATCH/single-stage/build
+
+  PIP=pip
+  PY=python
+  DEVICE=cuda
+  DEVICES=0,1
+  GPUS=all
+  WORKERS_TRAIN=8
+  WORKERS_INFER=8
+  DATASETS_DIR=$SCRATCH/datasets
 else
   ### Local
   PY=python
