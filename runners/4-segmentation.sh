016b08984177a86a27d89d5762955660384edd91
lucasdavid <1595030+lucasdavid@users.noreply.github.com>
2024-06-27 16:06:29 -0300

    Restore runners/segmentation

--- runners/4-segmentation.sh
+++ runners/4-segmentation.sh
@@ -4,7 +4,7 @@
 #SBATCH -p sequana_gpu_shared
 #SBATCH -J segm
 #SBATCH -o /scratch/lerdl/lucas.david/logs/%j-segm.out
-#SBATCH --time=10:00:00
+#SBATCH --time=96:00:00
 
 # Copyright 2023 Lucas Oliveira David
 #
@@ -44,21 +44,22 @@ cd $WORK_DIR
 export PYTHONPATH=$(pwd)
 
 
-# Architecture
-# ARCHITECTURE=deeplabv3p
-# ARCH=rs269
-# BACKBONE=resnest269
-# PRETRAINED_WEIGHTS=imagenet
-ARCHITECTURE=segformer
+## Architecture
+ARCHITECTURE=deeplabv3p
+ARCH=rs269
+BACKBONE=resnest269
+PRETRAINED_WEIGHTS=imagenet
+
+# ARCHITECTURE=segformer
 # BACKBONE=mit_b0
 # PRETRAINED_WEIGHTS=./experiments/models/pretrained/mit_b0.pth
 # ARCH=segformer_mit_b0
 # BACKBONE=mit_b2
 # PRETRAINED_WEIGHTS=./experiments/models/pretrained/mit_b2.pth
 # ARCH=segformer_mit_b2
-BACKBONE=mit_b5
-PRETRAINED_WEIGHTS=./experiments/models/pretrained/mit_b5.pth
-ARCH=segformer_mit_b5
+# BACKBONE=mit_b5
+# PRETRAINED_WEIGHTS=./experiments/models/pretrained/mit_b5.pth
+# ARCH=segformer_mit_b5
 
 # BACKBONE=swin_l
 # ARCH=swin_l_22k
@@ -67,21 +68,25 @@ ARCH=segformer_mit_b5
 
 GROUP_NORM=true
 DILATED=false
-MODE=normal # fix
+MODE=fix
+TRAIN_STEM=false
+TRAIN_BONE=true
 
 OPTIMIZER=momentum
-LR=0.01 # SegFormer
+# LR=0.01 # SegFormer
+# WD=0.01
+WD=4e-05
 # LR=0.007 # voc12
-# LR=0.004  # coco14
+LR=0.004  # coco14
 # LR=0.01  # deepglobe
-WD=0.01
 
-EPOCHS=50
+EPOCHS=15
 
-BATCH_SIZE=32
-ACCUMULATE_STEPS=1
+BATCH_SIZE=16
+ACCUMULATE_STEPS=2
 
 AUGMENT=none # colorjitter_randaug_cutmix_mixup_cutormixup
+AUG=no
 CUTMIX=0.5
 MIXUP=1.
 LABELSMOOTHING=0 # 0.1
@@ -103,7 +108,7 @@ segm_training() {
   echo "Semantic Segmentation Training $TAG"
   echo "================================================="
 
-  WANDB_TAGS="$DATASET,$ARCH,segmentation,b:$BATCH_SIZE,gn,lr:$LR,opt:$OPTIMIZER,ls:$LABELSMOOTHING,aug:$AUGMENT,mode:$MODE" \
+  WANDB_TAGS="$DATASET,$ARCH,segmentation,b:$BATCH_SIZE,gn,lr:$LR,opt:$OPTIMIZER,ls:$LABELSMOOTHING,aug:$AUG,mode:$MODE" \
     WANDB_RUN_GROUP="$DATASET-$ARCH-segmentation" \
     CUDA_VISIBLE_DEVICES=$DEVICES \
     $PY scripts/segmentation/train.py \
@@ -116,6 +121,8 @@ segm_training() {
     --mixed_precision $MIXED_PRECISION \
     --architecture $ARCHITECTURE \
     --backbone $BACKBONE \
+    --trainable-stem $TRAIN_STEM \
+    --trainable-backbone $TRAIN_BONE \
     --dilated $DILATED \
     --mode $MODE \
     --backbone_weights $PRETRAINED_WEIGHTS \
@@ -183,18 +190,18 @@ evaluate_masks() {
 ##
 
 LABELSMOOTHING=0.1
-AUGMENT=colorjitter # colorjitter_randaug_cutmix_mixup_cutormixup
+AUGMENT=colorjitter  # colorjitter_randaug_cutmix_mixup_cutormixup
 AUG=cj
 
 ## For supervised segmentation:
-PRIORS_TAG=sup
-MASKS_DIR=""
+# PRIORS_TAG=sup
+# MASKS_DIR=""
 
 ## For custom masks (pseudo masks from WSSS):
 # PRIORS_TAG=pnoc-rals-r4-ccamh-rw
 # MASKS_DIR=./experiments/predictions/rw/$DATASET-an@ccamh@rs269-pnoc-ls-r4@rs269-rals@beta=10@exp_times=8@rw@crf=1
 
-TAG=segmentation/$DATASET-$IMAGE_SIZE-$ARCH-lr$LR-ls-$MODE-$PRIORS_TAG
+TAG=segmentation/$DATASET-$IMAGE_SIZE-$ARCH-ls-lr$LR-b$BATCH_SIZE-$MODE-$AUG-$PRIORS_TAG
 segm_training
 
 # 4.2 DeepLabV3+ Inference
@@ -203,22 +210,10 @@ segm_training
 SEGM_PRED_DIR=./experiments/predictions/$TAG@crf=$CRF_T
 DOMAIN=$DOMAIN_VALID     segm_inference
 DOMAIN=$DOMAIN_VALID_SEG segm_inference
-DOMAIN=$DOMAIN_TEST SEGM_PRED_DIR=./experiments/predictions/$TAG@test@crf=$CRF_T segm_inference
+DOMAIN=$DOMAIN_TEST      SEGM_PRED_DIR=./experiments/predictions/$TAG@test@crf=$CRF_T segm_inference
 
 # 4.3. Evaluation
 #
-# DOMAIN=$DOMAIN_VALID     evaluate_masks
-# DOMAIN=$DOMAIN_VALID_SEG evaluate_masks
+DOMAIN=$DOMAIN_VALID     evaluate_masks
+DOMAIN=$DOMAIN_VALID_SEG evaluate_masks
 
-# TAG=segmentation/coco-481-d2-u2pl-rs269-sam
-# EVAL_MODE=png
-# ARCHITECTURE=deeplabv2
-# ARCH=dv2
-# IMAGE_SIZE=481
-# BATCH_SIZE=10
-# LABELSMOOTHING=0
-# AUGMENT=none
-# AUG=no
-# DOMAIN=$DOMAIN_VALID_SEG SEGM_PRED_DIR=./experiments/predictions/$TAG/logit CRF_T=0 evaluate_masks
-# DOMAIN=$DOMAIN_VALID_SEG SEGM_PRED_DIR=./experiments/predictions/$TAG/logit CRF_T=10 evaluate_masks
-# DOMAIN=$DOMAIN_VALID_SEG SEGM_PRED_DIR=./experiments/predictions/$TAG CRF_T=0 evaluate_masks
